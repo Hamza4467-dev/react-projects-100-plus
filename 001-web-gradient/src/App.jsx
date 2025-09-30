1a6ddb7b5bff0ea29e4c93fe18bff225a7470e13
mac <mac@macs-MacBook-Pro.local>
2025-09-30 12:57:41 +0500

    Test Push: Added Hello World code to Project 001 for workflow verification.

---
+++ 001-web-gradient/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Project 001 Test: Hello, I'm Working!</h1>
+      <p>This is the code for the Web Gradient Generator.</p>
+    </div>
+  );
+}
+
+export default App;
